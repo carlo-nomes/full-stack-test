0b8b1b2b5f09c307a0e7a87436ac971bcdf0a78d
carlo.nomes <nomes.carlo@gmail.com>
2022-12-01 14:28:00 +0100

    initialize react-ui

---
+++ react-ui/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+    return <div>{/* Your code here */}</div>;
+};
+
+export default App;
---
+++ react-ui/src/GlobalStyles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+// Reset CSS
+export const GlobalStyles = styled.div`
+    body {
+        /* System default fonts */
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+            'Helvetica Neue', sans-serif;
+
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+`;
