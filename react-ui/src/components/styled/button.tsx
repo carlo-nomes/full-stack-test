b55d44cc998897c4a3b02ab87221991db0ba245c
Jaimyschatteman <schattemanjaimy@outlook.com>
2024-01-12 11:52:27 +0100

    feat: add props to button

--- react-ui/src/components/styled/button.tsx
+++ react-ui/src/components/styled/button.tsx
@@ -1,16 +1,42 @@
 import styled from 'styled-components';
+import { focus } from './state';
 
-export const Button = styled.button`
-    background-color: #fff;
-    border: 1px solid #ccc;
+export const Button = styled.button<{
+    $appearance?: 'primary' | 'secondary' | 'text';
+    $size?: 'small' | 'medium' | 'large';
+    $fullWidth?: boolean;
+}>`
+    background-color: ${({ $appearance }) => ($appearance === 'primary' ? '#111' : '#fff')};
+    color: ${({ $appearance }) => ($appearance === 'primary' ? '#fff' : '#111')};
+    width: ${({ $fullWidth }) => ($fullWidth ? '100%' : 'auto')};
+    border: ${({ $appearance }) => ($appearance !== 'text' ? '2px solid #111' : 'none')};
+    padding: ${({ $size }) => {
+        switch ($size) {
+            case 'small':
+                return '0.5rem 1rem';
+            case 'large':
+                return '1.5rem 2rem';
+            default:
+                return '1rem 1.5rem';
+        }
+    }};
+    font-size: ${({ $size }) => {
+        switch ($size) {
+            case 'small':
+                return '0.75rem';
+            case 'large':
+                return '1.25rem';
+            default:
+                return '1rem';
+        }
+    }};
     border-radius: 4px;
-    color: #333;
     cursor: pointer;
-    font-size: 1rem;
     font-weight: 600;
-    line-height: 1.5;
-    padding: 0.5rem 1rem;
     text-decoration: none;
     text-transform: none;
     white-space: nowrap;
+    text-align: start;
+
+    ${focus}
 `;
