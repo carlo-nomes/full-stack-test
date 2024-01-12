a8328828649e0ff709cb1b5c273f8c4ee657245b
Jaimyschatteman <schattemanjaimy@outlook.com>
2024-01-12 11:47:54 +0100

    feat: add loading styled component

---
+++ react-ui/src/components/styled/loading.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default function Loading() {
+    return <Wrapper>Loading</Wrapper>;
+}
+
+export const Wrapper = styled.div`
+    font-family: 'InterDisplay', 'Inter', sans-serif;
+    font-size: 6rem;
+    line-height: 1.5;
+    font-weight: 800;
+`;
