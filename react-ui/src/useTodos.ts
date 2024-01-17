0c7d33efc7dfd7d0244da05ca77bfe4a63ba184e
Jasper Vermeulen <jaspervermeulen@icloud.com>
2024-01-17 11:09:56 +0100

    feat: todos hook

---
+++ react-ui/src/useTodos.ts
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+import { Todo } from './types/todo';
+import toast from 'react-hot-toast';
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+function useTodos() {
+    const [loading, setLoading] = useState(false);
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    async function getAllTodos() {
+        setLoading(true);
+        try {
+            const response = await fetch(`${API_URL}/todos`);
+            const data = await response.json();
+            setTodos(data);
+        } catch (error: unknown) {
+            toast.error('Something went wrong.');
+            setLoading(false);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    async function addTodo(title: string, priority: string) {
+        setLoading(true);
+        try {
+            const response = await fetch(`http://localhost:3000/todos`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ title, priority }),
+            });
+            const data = await response.json();
+            setTodos([...todos, data]);
+        } catch (error: unknown) {
+            toast.error('Something went wrong.');
+            setLoading(false);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const deleteTodo = async (id: string) => {
+        setLoading(true);
+        try {
+            const response = await fetch(`http://localhost:3000/todos/${id}`, {
+                method: 'DELETE',
+            });
+            if (!response.ok) throw response;
+            setTodos(todos.filter((todo) => todo.id !== id));
+        } catch (error: unknown) {
+            toast.error('Something went wrong.');
+            setLoading(false);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const toggleTodo = async (id: string) => {
+        setLoading(true);
+        try {
+            const response = await fetch(`http://localhost:3000/todos/${id}/toggle`, {
+                method: 'PATCH',
+            });
+            if (!response.ok) throw response;
+            const data = await response.json();
+            setTodos(todos.map((todo) => (todo.id === id ? data : todo)));
+        } catch (error: unknown) {
+            toast.error('Something went wrong.');
+            setLoading(false);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return [todos, { getAllTodos, addTodo, deleteTodo, toggleTodo }, { loading }] as const;
+}
+
+export default useTodos;
