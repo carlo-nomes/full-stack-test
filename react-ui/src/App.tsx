5d39fb2bbafcc8f6342d743794645bb39586eff6
Jasper Vermeulen <jaspervermeulen@icloud.com>
2024-01-17 11:11:01 +0100

    feat: add overview

--- react-ui/src/App.tsx
+++ react-ui/src/App.tsx
@@ -1,5 +1,27 @@
-const App = () => {
-    return <div>{/* Your code here */}</div>;
-};
+import { useEffect } from 'react';
+import useTodos from './useTodos';
 
-export default App;
+import './index.css';
+import List from './List';
+import Loader from './Loader';
+import Form from './Form';
+
+export default function App() {
+    const [todos, { getAllTodos, addTodo, deleteTodo, toggleTodo }, { loading }] = useTodos();
+
+    useEffect(() => {
+        getAllTodos();
+    }, []);
+
+    return (
+        <div className="max-w-screen-xl mx-auto border-x relative px-20 pt-20 min-h-screen">
+            <Loader loading={loading} />
+            <p className="text-lg font-medium">Add todo</p>
+            <Form addTodo={addTodo} loading={loading} />
+            <div className="flex flex-col gap-12 my-12">
+                <List title="Completed" todos={todos} filter={true} deleteTodo={deleteTodo} toggleTodo={toggleTodo}></List>
+                <List title="Not completed" todos={todos} filter={false} deleteTodo={deleteTodo} toggleTodo={toggleTodo}></List>
+            </div>
+        </div>
+    );
+}
--- react-ui/src/main.tsx
+++ react-ui/src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import { GlobalStyles } from './GlobalStyles';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
+        <Toaster />
         <GlobalStyles />
         <App />
     </React.StrictMode>,
